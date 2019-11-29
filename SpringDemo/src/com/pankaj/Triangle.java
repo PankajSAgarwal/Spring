7922f3e6067f54399b44d3e4667dd097de3e9293
Agarwal, Pankaj <2005pank@gmail.com>
2019-11-29 23:00:37 +0530

    Spring Autowire

--- SpringDemo/src/com/pankaj/Triangle.java
+++ SpringDemo/src/com/pankaj/Triangle.java
@@ -4,15 +4,52 @@ import java.util.List;
 
 public class Triangle {
 	
-	/*private Point pointA;
+	private Point pointA;
 	private Point pointB;
-	private Point pointC;*/
+	private Point pointC;
 	
-	private List<Point> points;
+	
+	
+	public Point getPointA() {
+		return pointA;
+	}
+
+
+
+	public void setPointA(Point pointA) {
+		this.pointA = pointA;
+	}
+
+
+
+	public Point getPointB() {
+		return pointB;
+	}
+
+
+
+	public void setPointB(Point pointB) {
+		this.pointB = pointB;
+	}
+
+
+
+	public Point getPointC() {
+		return pointC;
+	}
+
+
+
+	public void setPointC(Point pointC) {
+		this.pointC = pointC;
+	}
+
+
+	//private List<Point> points;
 	
 	
 
-	public List<Point> getPoints() {
+	/*public List<Point> getPoints() {
 		return points;
 	}
 
@@ -20,14 +57,16 @@ public class Triangle {
 
 	public void setPoints(List<Point> points) {
 		this.points = points;
-	}
+	}*/
 
 
 	public void draw() {
 		
-		for (Point point:points) {
-			System.out.println("Point  = ("+ point.getX() + " , " +point.getY() +")");
-		}
+		
+			System.out.println("Point  A= ("+ pointA.getX() + " , " +pointA.getY() +")");
+			System.out.println("Point  B= ("+ pointB.getX() + " , " +pointB.getY() +")");
+			System.out.println("Point  C= ("+ pointC.getX() + " , " +pointC.getY() +")");
+		
 		
 	}
 
