ccb0b600e18ac088fff4b9e605be504cd13db83a
Agarwal, Pankaj <2005pank@gmail.com>
2019-12-01 11:50:31 +0530

    Required annotation and RequiredAnnotationBeanPostProcessor in spring

--- SpringDemo/src/com/pankaj/DrawingApp.java
+++ SpringDemo/src/com/pankaj/DrawingApp.java
@@ -1,11 +1,7 @@
 package com.pankaj;
 
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.xml.XmlBeanFactory;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.core.io.FileSystemResource;
 
 @SuppressWarnings("deprecation")
 public class DrawingApp {
@@ -21,8 +17,10 @@ public class DrawingApp {
 		
 		AbstractApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
 		context.registerShutdownHook();
-		Shape shape = (Circle)context.getBean("circle");
+		Shape shape = (Shape)context.getBean("circle");
 		shape.draw();
+		
+		
 
 	}
 
