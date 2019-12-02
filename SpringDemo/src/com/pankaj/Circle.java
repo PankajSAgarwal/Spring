866f746a85993d724105da37e16ca53851fa92bf
Agarwal, Pankaj <2005pank@gmail.com>
2019-12-02 12:13:28 +0530

    Spring components and stereotypes

--- SpringDemo/src/com/pankaj/Circle.java
+++ SpringDemo/src/com/pankaj/Circle.java
@@ -6,7 +6,9 @@ import javax.annotation.Resource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
 
+@Component
 public class Circle implements Shape {
 
 
