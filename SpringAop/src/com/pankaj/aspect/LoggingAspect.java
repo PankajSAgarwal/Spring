995aade0b80d8ab14ed2fd16b30379a2553db888
Agarwal, Pankaj <2005pank@gmail.com>
2019-12-04 10:36:58 +0530

    Join Points getTarget method

--- SpringAop/src/com/pankaj/aspect/LoggingAspect.java
+++ SpringAop/src/com/pankaj/aspect/LoggingAspect.java
@@ -1,21 +1,29 @@
 package com.pankaj.aspect;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 
+import com.pankaj.model.Circle;
+
 @Aspect
 public class LoggingAspect {
 	
-	@Before("allGetters() && allCircleMethods()")
-	public void LoggingAdvice() {
-		System.out.println("Advice run . All circle Get method called .");
+	@Before("allCircleMethods()")
+	public void LoggingAdvice(JoinPoint joinPoint) {
+		//System.out.println("Advice run . All circle Get method called .");
+		System.out.println(joinPoint.toString());
+		System.out.println(joinPoint.getTarget());
+		
+		Circle circle = (Circle)joinPoint.getTarget();
+		
 	}
 	
-	@Before("allGetters()")
+	/*@Before("allGetters()")
 	public void secondAdvice() {
 		System.out.println("Second advice executed");
-	}
+	}*/
 	
 	@Pointcut("execution(public * get*(..))")
 	public void allGetters() {}
